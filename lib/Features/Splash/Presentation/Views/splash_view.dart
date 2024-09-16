ffdf6383669e277da0e9e85bb9608a465ee062bf
Beshoy Armia <beshoarmia0@gmail.com>
2024-09-16 13:48:50 +0300

    Refactor SplashView to use SplashViewBody widget
    Imported `SplashViewBody` widget and replaced the empty `Scaffold` in `SplashView` with `SplashViewBody`. This change improves the modularity and readability of the code by encapsulating the splash screen's UI logic into a separate widget.

--- lib/Features/Splash/Presentation/Views/splash_view.dart
+++ lib/Features/Splash/Presentation/Views/splash_view.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/Features/Splash/Presentation/Views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
@@ -5,6 +6,6 @@ class SplashView extends StatelessWidget {
   static const routeName = 'splash_view';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const SplashViewBody();
   }
 }
