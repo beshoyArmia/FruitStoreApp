9e09c835546adc0be750b33d3c3037210cc81cc5
Beshoy Armia <beshoarmia0@gmail.com>
2024-09-16 14:56:34 +0300

    Refactor SplashViewBody to StatefulWidget and add navigation to OnBoardingView
    - Converted `SplashViewBody` from a `StatelessWidget` to a `StatefulWidget` to manage state and handle navigation.
    - Implemented `initState` to delay navigation to `OnBoardingView` after 3 seconds.
    - Added `OnBoardingView` as a new route and registered it in the `onGenerateRoute` function.
    - Updated `SplashViewBody` to use `Navigator.pushReplacementNamed` for navigation to `OnBoardingView`.

--- lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
@@ -1,11 +1,23 @@
+import 'package:e_commerce/Features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:e_commerce/core/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:svg_flutter/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  @override
+  void initState() {
+    excute();
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -20,5 +32,11 @@ class SplashViewBody extends StatelessWidget {
         SvgPicture.asset(Assets.imagesButtonSplash),
       ],
     );
+  }
+
+  void excute() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+    });
   }
 }
--- lib/core/helper_function/on_generate_routes.dart
+++ lib/core/helper_function/on_generate_routes.dart
@@ -1,10 +1,13 @@
 import 'package:e_commerce/Features/Splash/Presentation/Views/splash_view.dart';
+import 'package:e_commerce/Features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
